59d50125d040c5b905ae5466a106becedcc29541
Josias <josiastech@gmail.com>
2017-06-18 23:09:46 -0600

    Update toast emthod

--- app/src/main/java/com/example/josias/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/josias/myfirstapp/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.josias.myfirstapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,4 +12,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /**
+     * Show a toast
+     * @param view -- the view that is clicked
+     */
+    public void toastMe(View view){
+        // Toast myToast = Toast.makeText(this, message, duration);
+        Toast myToast = Toast.makeText(this, "Hello Toast!",
+                Toast.LENGTH_SHORT);
+        myToast.show();
+    }
+
 }
